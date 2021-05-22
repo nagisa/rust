81c42c73fd613e9072d1745caf22c82f232baafb
Théo Degioanni <degiotheo@gmail.come>
2021-05-22 23:03:09 +0200

    add a simplication pass for constprop

--- compiler/rustc_mir/src/transform/mod.rs
+++ compiler/rustc_mir/src/transform/mod.rs
@@ -501,6 +501,7 @@ fn run_optimization_passes<'tcx>(tcx: TyCtxt<'tcx>, body: &mut Body<'tcx>) {
         &multiple_return_terminators::MultipleReturnTerminators,
         &instcombine::InstCombine,
         &separate_const_switch::SeparateConstSwitch,
+        &simplify::SimplifyCfg::new("before-const-prop"),
         &const_prop::ConstProp,
         &simplify_branches::SimplifyBranches::new("after-const-prop"),
         &early_otherwise_branch::EarlyOtherwiseBranch,
